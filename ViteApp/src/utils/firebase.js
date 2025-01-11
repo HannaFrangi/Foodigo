3a9a87f6dffaf6a1e4e155e9675919ef007d4cdd
Hanna Frangi <HannaFrangi.hf@gmail.com>
2025-01-11 21:30:45 +0200

    Refactor Firebase messaging setup: update imports and register background message handler

--- ViteApp/src/utils/firebase.js
+++ ViteApp/src/utils/firebase.js
@@ -1,42 +1,26 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-
 import { getAnalytics } from "firebase/analytics";
-
-import { getMessaging, onBackgroundMessage } from "firebase/messaging";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getMessaging, onMessage } from "firebase/messaging";
+import { onBackgroundMessage } from "firebase/messaging/sw"; // Corrected import for background message handling
 
 // Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: "AIzaSyCYUIfiZEdqmr2XbdSP0tdtMrAiAeL6IaM",
-
   authDomain: "hdarne-3d2b6.firebaseapp.com",
-
   projectId: "hdarne-3d2b6",
-
   storageBucket: "hdarne-3d2b6.appspot.com",
-
   messagingSenderId: "137464243804",
-
   appId: "1:137464243804:web:933b0aeddfd4c6d72ca899",
-
   measurementId: "G-TT3XYP63T1",
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
-
 const analytics = getAnalytics(app);
 const messaging = getMessaging(app);
 
+// Register the background message handler
 onBackgroundMessage(messaging, (payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message: ",
@@ -52,4 +36,5 @@ onBackgroundMessage(messaging, (payload) => {
 
   self.registration.showNotification(title, notificationOptions);
 });
+
 export { messaging, app };
