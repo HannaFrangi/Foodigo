5172bbfc984672007b3cf9afbce4ab56d73b9fc9
Hanna Frangi <HannaFrangi.hf@gmail.com>
2025-01-11 22:03:18 +0200

    Register service worker for Firebase messaging in main application file

--- ViteApp/src/main.jsx
+++ ViteApp/src/main.jsx
@@ -16,6 +16,17 @@ const updateSW = registerSW({
     console.log("App ready to work offline");
   },
 });
+// index.js (or App.jsx)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/firebase-messaging-sw.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed: X", error);
+    });
+}
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
