75d740977d900534b70cad969bcff3d9fcda78e9
Hanna Frangi <HannaFrangi.hf@gmail.com>
2025-01-30 17:17:13 +0200

    Add production URL to CORS configuration for API

--- api/server.js
+++ api/server.js
@@ -22,7 +22,11 @@ const PORT = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://foodigo-tau.vercel.app",
+    ],
     credentials: true,
   })
 );
